147235a0e6eaa8a3b53a5f4c52cee7929468ff93
Dmitriy <dmkrch1@gmail.com>
2020-11-17 13:03:28 +0300

    1 more signal to handle

--- SignalProcessInteraction/SignalProcessInteraction.c
+++ SignalProcessInteraction/SignalProcessInteraction.c
@@ -6,13 +6,14 @@
 static int n = 0;
 const char message[] = "ctrl_slash\n";
 const char message1[] = "ctrl_z\n";
+const char message2[] = "ctrl_c\n";
 
 
 void ctrl_z_handler(int s)
 {
     int save_errno = errno;
     signal(SIGTSTP, ctrl_z_handler);
-    
+
     n++;
     write(1, message1, sizeof(message1)-1);
 
@@ -30,11 +31,23 @@ void ctrl_slash_handler(int s)
     errno = save_errno;
 }
 
+void ctrl_c_handler(int s)
+{
+    int save_errno = errno;
+    signal(SIGINT, ctrl_c_handler);
+
+    n++;
+    write(1, message2, sizeof(message2)-1);
+
+    errno = save_errno;
+}
+
 
 int main()
 {
     signal(SIGQUIT, ctrl_slash_handler);
     signal(SIGTSTP, ctrl_z_handler);
+    signal(SIGINT, ctrl_c_handler);
 
     while(n < 25)
         pause();
