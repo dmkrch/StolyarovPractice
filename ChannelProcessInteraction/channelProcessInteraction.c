d91eb16921dc7cfbbf331cb1d536bbddeb8d95ea
Dmitriy <dmkrch1@gmail.com>
2020-11-17 14:34:15 +0300

    pure example. needs to be fixed

---
+++ ChannelProcessInteraction/channelProcessInteraction.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd[2];
+    int p1, p2;
+    char* data = (char*)malloc(100*sizeof(char));
+
+    pipe(fd);
+    p1 = fork();
+
+    if (p1 == 0) // child #1
+    {
+        char dataWrite[] = "hello\n";
+        close(fd[0]);
+        write(fd[1], dataWrite, 6);
+
+        exit(0);
+    }
+
+    p2 = fork();
+
+    if (p2 == 0) // child #2
+    {
+        close(fd[1]);
+
+        read(fd[0], data, 6);
+
+        exit(0);
+    }
+
+
+    /* parent */
+    close(fd[0]);
+    close(fd[1]);
+    
+    printf("%s",data);
+
+}
