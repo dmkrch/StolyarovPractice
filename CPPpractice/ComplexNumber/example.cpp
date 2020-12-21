07f5d5e3476f9d70760f55beaef71cf46bc8cc4b
Dmitriy <dmkrch1@gmail.com>
2020-12-21 12:26:43 +0300

    complex number, overloading operators practice

---
+++ CPPpractice/ComplexNumber/example.cpp
@@ -0,0 +1,50 @@
+#include <math.h>
+#include <stdio.h>
+
+class Complex
+{
+    private:
+        double re, im;
+    public:
+        Complex(double a_re, double a_im)
+        {
+            re = a_re;
+            im = a_im;
+        }
+
+        double Modulo() { return sqrt(re*re + im*im); }
+        double Argument() { return atan2(im, re); }
+        double GetRe() { return re; }
+        double GetIm() { return im; }
+        
+        Complex operator + (Complex op2)
+        {
+            Complex res(re + op2.re, im + op2.im);
+            return res;
+        }
+
+        Complex operator - (Complex op2)
+        {
+            Complex res(re - op2.re, im - op2.im);
+            return res;
+        }
+
+        Complex operator * (Complex op2)
+        {
+            Complex res(re*op2.re - im*op2.im, re*op2.im + im*op2.re);
+            return res;
+        }
+
+        Complex operator / (Complex op2)
+        {
+            double dvs = op2.re*op2.re + op2.im*op2.im;
+            Complex res((re*op2.re + im*op2.im) / dvs,
+                        (im*op2.re - re*op2.im) / dvs);
+            return res;
+        }
+};
+
+int main()
+{
+    return 0;
+}
